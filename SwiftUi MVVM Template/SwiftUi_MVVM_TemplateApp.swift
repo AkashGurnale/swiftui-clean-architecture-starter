cff8deead13a69c61a349fb29f2a16a91e0df90c
Akash Gurnale <akash.gurnale@gmail.com>
2025-10-16 20:51:42 +0530

    Initial Commit

---
+++ SwiftUi MVVM Template/SwiftUi_MVVM_TemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUi_MVVM_TemplateApp.swift
+//  SwiftUi MVVM Template
+//
+//  Created by Akash on 16/10/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUi_MVVM_TemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
